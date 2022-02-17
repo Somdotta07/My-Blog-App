00577e8403deeed287fc3cbbe43f33a2a2b01aa4
Somdotta07 <jhilick.s@gmail.com>
2022-02-17 21:01:37 +0100

    fix issues

--- db/schema.rb
+++ db/schema.rb
@@ -18,19 +18,19 @@ ActiveRecord::Schema[7.0].define(version: 2022_02_17_122345) do
     t.text "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "posts_id", null: false
+    t.bigint "post_id", null: false
     t.bigint "author_id", null: false
     t.index ["author_id"], name: "index_comments_on_author_id"
-    t.index ["posts_id"], name: "index_comments_on_posts_id"
+    t.index ["post_id"], name: "index_comments_on_post_id"
   end
 
   create_table "likes", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "post_id", null: false
     t.bigint "author_id", null: false
-    t.bigint "posts_id", null: false
     t.index ["author_id"], name: "index_likes_on_author_id"
-    t.index ["posts_id"], name: "index_likes_on_posts_id"
+    t.index ["post_id"], name: "index_likes_on_post_id"
   end
 
   create_table "posts", force: :cascade do |t|
@@ -40,8 +40,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_02_17_122345) do
     t.integer "likes_counter", default: 0, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "author_id", null: false
-    t.index ["author_id"], name: "index_posts_on_author_id"
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -57,5 +57,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_02_17_122345) do
   add_foreign_key "comments", "users", column: "author_id"
   add_foreign_key "likes", "posts"
   add_foreign_key "likes", "users", column: "author_id"
-  add_foreign_key "posts", "users", column: "author_id"
+  add_foreign_key "posts", "users"
 end
