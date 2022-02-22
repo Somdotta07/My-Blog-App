a02c7902e8ded987443fd0d5e4f6254a1ac9cfcc
Somdotta07 <jhilick.s@gmail.com>
2022-02-22 15:11:38 +0100

    Add user HTML

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,6 @@
 class Comment < ApplicationRecord
-  belongs_to :author, class_name: 'User'
-  belongs_to :post
+  belongs_to :user
+  belongs_to :post, foreign_key: 'post_id'
 
   def update_comments_counter
     post.increment!(:comments_counter)
