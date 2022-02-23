0621fb6bc159d38fe95e67f870d87da1caa668d9
Somdotta07 <jhilick.s@gmail.com>
2022-02-23 11:32:06 +0100

    Fix styles and bugs

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,8 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :post, foreign_key: 'post_id'
 
+  after_save :update_comments_counter
+
   def update_comments_counter
     post.increment!(:comments_counter)
   end
