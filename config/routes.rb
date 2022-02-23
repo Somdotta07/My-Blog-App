7806e5b5a051dedefcaffd5890e2cdb68e1ce3e9
Somdotta07 <jhilick.s@gmail.com>
2022-02-23 19:21:14 +0100

    Add routes for form

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'users#index'
   resources :users, only: %i[index show] do
-    resources :posts, only: %i[index show]
+    resources :posts, only: %i[new create index show] do
+      get 'comments/new', to: 'comments#new'
+      post 'comments/new', to: 'comments#create'
+      post 'likes/new', to: 'likes#create'
+    end
   end
 end
