26901c091149620b959b14c431289e7e11d50820
Somdotta07 <jhilick.s@gmail.com>
2022-02-25 00:42:08 +0100

    add the permitted params into a private method

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -15,18 +15,24 @@ class PostsController < ApplicationController
   end
 
   def create
-    @new_post = current_user.posts.new(params.require(:post).permit(:title, :text))
+    @new_post = current_user.posts.new(post_params)
     @new_post.likes_counter = 0
     @new_post.comments_counter = 0
     @new_post.update_posts_counter
     respond_to do |format|
       format.html do
         if @new_post.save
-          redirect_to "/users/#{@new_post.user.id}/posts/", notice: 'Successfully saved !'
+          redirect_to "/users/#{@new_post.user.id}/posts/", notice: 'Success!'
         else
           render :new, alert: 'Error occured!'
         end
       end
     end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :text)
   end
 end
