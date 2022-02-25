0a54560733bc3176706625100f71ce807c8d1398
Somdotta07 <jhilick.s@gmail.com>
2022-02-25 14:56:19 +0100

    Update post_spec.rb

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Post model' do
+    subject { Post.new(title: 'New post', text: 'Hi there', user_id: 2) }
+    before { subject.save }
+
+    it 'check if the title is not blank' do
+      subject.title = nil
+      expect(subject).to_not be_valid
+    end
+
+    it 'check if the title is not exceeding 250 characters' do
+      subject.title = 'Hello world Hello world Hello world Hello world Hello world Hello world Hello world Hello world
+      Hello world Hello world Hello world Hello world Hello world Hello world Hello world Hello world Hello world Hello
+      world Hello world Hello world Hello world Hello world Hello world Hello world Hello world Hello world Hello world '
+      expect(subject).to_not be_valid
+    end
+
+    it 'check if it increases the number of comments' do
+      prev_posts_counter = User.find(1).posts_counter
+      subject.update_posts_counter
+      expect(User.find(1).posts_counter).to eq(prev_posts_counter + 1)
+    end
+  end
 end
