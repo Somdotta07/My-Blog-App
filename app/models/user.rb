8aaf66bad81415b6381d5d969ed6ab284cf97bad
Simo Nkosi <wilzmar.kwd@gmail.com>
2022-03-02 16:00:00 +0200

    Correct rubocop offenses

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,6 @@ class User < ApplicationRecord
   has_many :likes, foreign_key: 'user_id'
   has_many :posts, foreign_key: 'user_id'
 
-
   def recent_posts
     posts.limit(3).order(created_at: :desc)
   end
